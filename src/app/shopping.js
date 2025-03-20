e9137ae5a9b27bd3d0d00229af5f634754d79b87
Balram-Choudhary1 <balramchoudhary1608@.com>
2025-03-20 16:17:54 +0530

    changes

---
+++ src/app/shopping.js
@@ -0,0 +1,26 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const shopping = () => {
+  return (
+    <View style={{flex:1 }}>
+         <View style={{  backgroundColor:'red', height:150,
+    borderBottomEndRadius:12, borderBottomStartRadius:12}}>
+
+      <View  >
+          <View>
+            <Text style={{  color:'#fff', backgroundColor:'green'}}>neargud</Text>
+          </View>
+
+          <View>
+            <Text>neargud</Text>
+          </View>
+      </View>
+
+    </View>
+    </View>
+   
+  )
+}
+
+export default shopping
